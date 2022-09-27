0772d2a5c9113f207efdd45cb11de2cc029c6c0b
Demi Hayashi <demi.h@me.com>
2022-09-27 12:34:47 -0700

    create light mode theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,14 @@ function pageTransitions() {
             element.classList.add('active');
         }
     })
+
+    // toggle between light & dark mode
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+
+        element.classList.toggle('light-mode');
+    })
 };
 
 pageTransitions();
