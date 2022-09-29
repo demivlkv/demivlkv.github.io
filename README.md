11c42bc3ad36f2e0c02a6210ad7a51a7d07baad6
Demi Hayashi <demi.h@me.com>
2022-09-28 20:30:22 -0700

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# portfolio-v2
+# v2: Portfolio
+<p align="center">:sparkles: Second rendition of my web developer portfolio. :sparkles:</p>
+
+[Version 1 here.](https://demivlkv.github.io/portfolio/)
+
+![This is a screenshot of my portfolio in dark mode.](/../main/assets/images/screenshot-dark-mode.png)
+
+![This is a screenshot of my portfolio in light mode.](/../main/assets/images/screenshot-light-mode.png)
+
+### Built With:
+- HTML
+- CSS / [Bootstrap](https://getbootstrap.com/)
+- JavaScript / [jQuery](https://jquery.com/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// display or hide navbar when scrolling
+// display navbar when scrolling up; hide navbar when scrolling down
 document.addEventListener("DOMContentLoaded", function(){
     const autoHideEl = document.querySelector('.auto-hide');
   
@@ -23,64 +23,7 @@ document.addEventListener("DOMContentLoaded", function(){
     };
 }); 
 
-// $(document).ready(function() {
-//     // sticky navigation menu
-//     let navOffset = $('.header').height() + 50;
-
-//     function fixedNavbar() {
-//         if ($('.header').length) {
-//             $(window).scroll(function() {
-//                 let scroll = $(window).scrollTop();
-//                 if (scroll >= navOffset) {
-//                     $('.header .main-menu').addClass('navbar-fixed');
-//                 } else {
-//                     $('.header .main-menu').removeClass('navbar-fixed');
-//                 }
-//             })
-//         }
-//     }
-
-//     fixedNavbar();
-
-// });
-
-// const siteMain = document.querySelector('.site-main');
-// const sections = document.querySelectorAll('.section');
-// const sectionBtns = document.querySelectorAll('.side-menu');
-// const sectBtn = document.querySelectorAll('.icon-menu');
-
-function pageTransitions() {
-    // create active class for side-menu button click
-    // for (let i = 0; i < sectBtn.length; i++) {
-    //     sectBtn[i].addEventListener('click', function() {
-    //         let currentBtn = document.querySelectorAll('.active-btn');
-
-    //         currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-    //         this.className += ' active-btn';
-    //     })
-    // }
-
-    // activate sections
-    // siteMain.addEventListener('click', (e) => {
-    //     const id = e.target.dataset.id;
-
-    //     if (id) {
-    //         // add 'active' class to target button
-    //         sectionBtns.forEach(btn => {
-    //             btn.classList.remove('active');
-    //         })
-    //         e.target.classList.add('active');
-
-    //         // hide other sections
-    //         sections.forEach((section) => {
-    //             section.classList.remove('active');
-    //         })
-
-    //         const element = document.getElementById(id);
-    //         element.classList.add('active');
-    //     }
-    // })
-
+function lightMode() {
     // toggle between light & dark mode
     const themeBtn = document.querySelector('.theme-btn');
     themeBtn.addEventListener('click', () => {
@@ -90,4 +33,4 @@ function pageTransitions() {
     })
 };
 
-pageTransitions();
+lightMode();
