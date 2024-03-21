853c87eb45762499b1a865f7144c5e0ce59bf1be
demivlkv <demi.h@me.com>
2024-03-20 18:58:57 -0700

    update dark mode colors

--- src/components/Icons/FastForward.jsx
+++ src/components/Icons/FastForward.jsx
@@ -9,7 +9,7 @@ const FastForward = () => {
 				stroke-width='2'
 				stroke-linecap='round'
 				stroke-linejoin='round'
-				className='inline-flex justify-center items-center pb-1 mr-1 text-emerald-400 dark:text-indigo-500'
+				className='inline-flex justify-center items-center pb-1 mr-1 text-emerald-400 dark:text-teal-200'
 			>
 				<use href='/feather-sprite.svg#fast-forward' />
 			</svg>
